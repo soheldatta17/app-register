4cc8b7b947befec9a5c0204b0253e409db693306
soheldatta17 <soheldatta17@gmail.com>
2024-01-06 23:46:58 +0530

    .

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,6 @@ import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
-import "../../App.css"
 
 function Home() {
   return (
@@ -12,32 +11,34 @@ function Home() {
       <Container fluid className="home-section" id="home">
         <Particle />
         <Container className="home-content">
-        <header>
-                <div className="section__container header__container">
-                    <div className="header__image">
+          <Row>
+            <Col md={7} className="home-header">
+              <h1 style={{ paddingBottom: 15 }} className="heading">
+                Hi There!{" "}
+                <span className="wave" role="img" aria-labelledby="wave">
+                  👋🏻
+                </span>
+              </h1>
 
-                    </div>
-                    <div className="header__content">
-                        <div>
-                            <p className="sub__header">Dine along us</p>
-                            <h1>Welcome to the <br />Foodies</h1>
-                            <p className="section__subtitle">
-                                A feast for the eyes, a symphony for the senses.
-                            </p>
-                            <div className="action__btns">
-                                <button className="btn">Start planning</button>
-                                <div className="story">
-                                    <div className="video__image">
-                                        {/* <img src={storyImage} alt="story" /> */}
-                                        <span><i className="ri-play-fill"></i></span>
-                                    </div>
-                                    <span>View food reviews</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </header>
+              <h1 className="heading-name">
+                I'M
+                <strong className="main-name"> SOHEL DATTA</strong>
+              </h1>
+
+              <div style={{ padding: 50, textAlign: "left" }}>
+                <Type />
+              </div>
+            </Col>
+
+            <Col md={5} style={{ paddingBottom: 20 }}>
+              <img
+                src={homeLogo}
+                alt="home pic"
+                className="img-fluid"
+                style={{ maxHeight: "450px",marginTop:"3em" }}
+              />
+            </Col>
+          </Row>
         </Container>
       </Container>
       <Home2 />
